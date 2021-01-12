97e579ac6b00abbda226f14aac8d249988478dc5
Ayu Sudi <ayusudi.abc@gmail.com>
2021-01-12 09:15:22 +0700

    null

---
+++ 2021/week1/1.swift
@@ -0,0 +1,21 @@
+let name : String
+name = "Oke"
+
+
+let status : Bool
+status = true
+
+let number : Int
+number = 12
+
+let doubleNumber : Double = 12345.789123
+
+let floatNumber : Float = 12345.789123
+
+
+print(name)
+print(status)
+print(number)
+print(doubleNumber)
+print(floatNumber)
+
---
+++ 2021/week1/2-array.swift
@@ -0,0 +1,12 @@
+let songs : [String] = ["Shake it Off", "You Belong with Me", "Love Story"]
+let songs2 : [String] = ["Today was a Fairytale", "Welcome to New York", "Fifteen"]
+let both : [Any] = songs + songs2
+
+
+print(both)
+print(both.count)
+
+print(songs[0])
+print(songs[0].count)
+
+
